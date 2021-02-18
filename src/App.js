3c3f73d135cad9277ac706368ac5b38e6de35cdf
srimee <junnamma@yahoo.com>
2021-02-18 13:00:28 -0500

    Changed contents of the app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import profileImage from './Img/profile-pic.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>First React App</h1>
+      <h2>Author: Meena</h2>
+      <img src={profileImage} className="profile-image" alt="profile-image" />
     </div>
   );
 }
